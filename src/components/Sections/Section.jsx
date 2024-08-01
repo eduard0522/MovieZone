b4b2fb7d354f98660c87157ba6b0592c6cb5afcd
eduard villamil <villamilandrey02@gmail.com>
2024-07-31 23:15:24 -0500

    se agregó sección de mas populares, falta culminar detalles

--- src/components/Sections/Section.jsx
+++ src/components/Sections/Section.jsx
@@ -5,10 +5,45 @@ const Section = () => {
     <section>
       <h2 className="text-3xl font-semibold mb-4 "> Most Popular </h2>
       <div className="flex gap-4 flex-wrap">
-        <div className="w-52 h-72 relative">
-          <img src="https://picsum.photos/208/288" alt="Card image cap" />
+        <div className="w-52 h-72 relative overflow-hidden cursor-pointer">
+          <img src="http://es.web.img3.acsta.net/pictures/17/11/27/13/44/5907148.jpg" alt="Card image cap" />
           <div className="w-full h-full absolute top-0 left-0 bg-gradient-to-t from-black to-transparent flex items-end">
-            <h5 className="mb-2 text-2xl font-normal px-4"> Card title </h5>
+            <h5 className="mb-2 text-2xl font-normal px-4"> Ferdinand </h5>
+          </div>
+        </div>
+
+        <div className="w-52 h-72 relative overflow-hidden cursor-pointer">
+          <img src="https://im.ziffdavisinternational.com/ign_es/screenshot/default/guardaines_jhsk.jpg" alt="Card image cap" />
+          <div className="w-full h-full absolute top-0 left-0 bg-gradient-to-t from-black to-transparent flex items-end">
+            <h5 className="mb-2 text-2xl font-normal px-4"> Guardianes de la Galaxía  2</h5>
+          </div>
+        </div>
+
+        <div className="w-52 h-72 relative overflow-hidden cursor-pointer">
+          <img src="https://www.themoviedb.org/t/p/original/v5aC4nrzXFGJDWY4JO1eengXzqk.jpg" alt="Card image cap" />
+          <div className="w-full h-full absolute top-0 left-0 bg-gradient-to-t from-black to-transparent flex items-end">
+            <h5 className="mb-2 text-2xl font-normal px-4"> Bolt </h5>
+          </div>
+        </div>
+
+        <div className="w-52 h-72 relative overflow-hidden cursor-pointer">
+          <img src="https://www.mubis.es/media/users/3724/203627/accion-cine-portada-y-posters-mayo-2018-l_cover.jpg" alt="Card image cap" />
+          <div className="w-full h-full absolute top-0 left-0 bg-gradient-to-t from-black to-transparent flex items-end">
+            <h5 className="mb-2 text-2xl font-normal px-4"> Deadpool 2</h5>
+          </div>
+        </div>
+
+        <div className="w-52 h-72 relative overflow-hidden cursor-pointer">
+          <img src="https://www.laguiadelvaron.com/wp-content/uploads/2019/07/portadas-pel%C3%ADculas-iguales-www.laguiadelvaron-4.jpg" alt="Card image cap" />
+          <div className="w-full h-full absolute top-0 left-0 bg-gradient-to-t from-black to-transparent flex items-end">
+            <h5 className="mb-2 text-2xl font-normal px-4"> Visitation </h5>
+          </div>
+        </div>
+
+        <div className="w-52 h-72 relative overflow-hidden cursor-pointer">
+          <img src="http://es.web.img2.acsta.net/pictures/19/05/27/12/05/3089180.jpg" alt="Card image cap" />
+          <div className="w-full h-full absolute top-0 left-0 bg-gradient-to-t from-black to-transparent flex items-end">
+            <h5 className="mb-2 text-2xl font-normal px-4"> Jhon Wick 3</h5>
           </div>
         </div>
     
