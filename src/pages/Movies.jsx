6e5804c2587b2bc1bd2570397626e5bdfcbd9894
eduard villamil <villamilandrey02@gmail.com>
2024-09-05 08:41:45 -0500

    se agregó footer

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,11 +1,28 @@
 import Nav from "../components/Nav/Nav"
+import { useContext } from "react"
+import { ContextVideo } from "../components/Contexts/contextVideo"
+
+import Movie from "../components/Movie/CardMovie"
 
 const Movies = () => {
+
+  const { movieList } = useContext(ContextVideo)
   return (
-    <section>
+    <>
       <Nav />
-      <h1 className=" text-4xl font-bold"> Series </h1>
-    </section>
+      <main>
+         <h1 className=" text-4xl font-bold"> All movies </h1>
+         <div className="flex justify-center flex-wrap mt-8 gap-4">
+            {
+              movieList.map(movie => {
+                return(
+                  <Movie key={movie.codeVideo} movie={movie} />
+                )
+              })
+            }
+         </div>
+      </main>
+    </>
   )
 }
 
